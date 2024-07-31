cf870b69ca0beb5b08c78c07f4f61dc8ab69e48d
tomason182 <elotro182@hotmail.com>
2024-07-31 12:46:58 -0300

    add: User login schema

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -64,6 +64,38 @@ const userRegisterSchema = {
   },
 };
 
+const userLoginSchema = {
+  username: {
+    in: ["body"],
+    isEmail: {
+      bail: true,
+      errorMessage: "username is not a valid email",
+    },
+  },
+  password: {
+    in: ["body"],
+    isStrongPassword: {
+      options: {
+        minLength: 14,
+        minLowerCase: 4,
+        minUppercase: 2,
+        minNumbers: 2,
+        minSymbols: 2,
+      },
+    },
+    custom: {
+      options: (value) => {
+        if (/\s/.test(value)) {
+          throw new Error("Password should not contain white spaces");
+        }
+        return true;
+      },
+    },
+    errorMessage:
+      "Password should contain at least 14 characters, 4 lowercase, 2 uppercase, 2 numbers and 2 symbols ",
+  },
+};
+
 // Middleware to sanitize body
 const sanitizeBody = function (req, res, next) {
   const allowedFields = [
