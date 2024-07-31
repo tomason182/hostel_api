fc5ac74adb1ebf8ff5720c3f2cd68c443b0e1db6
tomason182 <elotro182@hotmail.com>
2024-07-31 12:30:32 -0300

    add passport config to app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const passport = require("./config/passport");
 
 // Require Errors middleware
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
@@ -12,10 +13,12 @@ const userRoutes = require("./routes/v1/userRoutes");
 
 const app = express();
 
+passport();
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Use routes
