f580c0fb608bc6fe64260aaca7fb68cd6e226180
tomason182 <elotro182@hotmail.com>
2024-07-31 12:19:44 -0300

    add: Passport-jwt strategy config

---
+++ src/config/passport.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const { ExtractJwt, Strategy } = require("passport-jwt");
+const {
+  connectToDatabase,
+  usersCollection,
+  closeConn,
+} = require("./db_config");
+
+function cookieExtractor(req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.signedCookies["jwt"];
+  }
+  return token;
+}
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromExtractors([
+    cookieExtractor,
+    fromAuthHeaderAsBearerToken(),
+  ]),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+const jwtStrategy = new Strategy(jwtOptions, async function (
+  jwt_payload,
+  done
+) {
+  await connectToDatabase();
+  const user = await usersCollection.findOne(
+    { _id: jwt_payload.sub },
+    { $project: { hashedPassword: 0, salt: 0 } }
+  );
+  await closeConn();
+  if (user === null) {
+    return null, false;
+  }
+
+  return done(null, user);
+});
+
+module.exports = (passport) => passport.use(jwtStrategy);
