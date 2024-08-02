9391383a0b2fa3d89b6699187c4d44e849a9a513
tomason182 <elotro182@hotmail.com>
2024-08-02 10:05:19 -0300

    fix: Configuración de conexión de bd
    Se modifico la configuración de la conexión a la base de datos para que trabaje correctamente con mongoMemoryServer

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -4,15 +4,18 @@ const uri = process.env.MONGO_URI;
 
 // Create mongo client instance
 const client = new MongoClient(uri);
+let db = null;
 
 // Database name
-const dbname = process.env.NODE_ENV === "test" ? "test" : process.env.DB_NAME;
+const dbname = process.env.DB_NAME;
 // Data collections
-const usersCollection = client.db(dbname).collection("users");
+// const usersCollection = client.db(dbname).collection("users");
 
 const connectToDatabase = async () => {
   try {
+    if (db) return;
     await client.connect();
+    db = client.db(dbname);
     console.log(`Connected to database ${dbname}`);
   } catch (err) {
     console.error(`Error connecting to the database: ${err}`);
@@ -20,9 +23,20 @@ const connectToDatabase = async () => {
   }
 };
 
-const closeConn = async () => {
-  await client.close();
-  console.log("connection Close");
+const setDb = (database) => {
+  db = database;
 };
 
-module.exports = { connectToDatabase, usersCollection, closeConn };
+const getDd = () => db;
+
+const closeConn = async () => {
+  try {
+    await client.close();
+    console.log("connection Close");
+  } catch (err) {
+    console.error("Unable to close conn to database");
+    process.exit(1);
+  }
+};
+
+module.exports = { connectToDatabase, closeConn, setDb, getDd };
