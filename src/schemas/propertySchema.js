386ac898c5cb816366ab7f4d3f9a9291288879f6
tomason182 <elotro182@hotmail.com>
2024-08-02 15:21:16 -0300

    add: esquema de validacion propiedad

--- src/schemas/propertySchema.js
+++ src/schemas/propertySchema.js
@@ -7,66 +7,59 @@ const propertySchema = {
       bail: true,
       errorMessage: "Property name must not be empty",
     },
-    isAlpha: {
-      errorMessage: "Property name must contain only alphabetic characters",
-    },
-    street: {
-      in: ["body"],
-      trim: true,
-      escape: true,
-      notEmpty: {
-        bail: true,
-        errorMessage: "Address is required",
-      },
-      isAlphanumeric: {
-        errorMessage: "Address should be alphanumeric",
-      },
-    },
-    city: {
-      in: ["body"],
-      trim: true,
-      escape: true,
-      notEmpty: {
-        bail: true,
-        errorMessage: "Address is required",
-      },
-      isAlpha: {
-        errorMessage: "Address should contain alphabetic characters",
-      },
-    },
-    postalCode: {
-      in: ["body"],
-      optional: true,
-      isPostalCode: {
-        options: "any",
-        errorMessage: "invalid postal code",
-      },
-    },
-    countryCode: {
-      in: ["body"],
-      isISO31661Alpha2: {
-        errorMessage: "Invalid country code",
-      },
-      notEmpty: {
-        bail: true,
-        errorMessage: "Country code is required",
-      },
-    },
-    phoneNumber: {
-      in: ["body"],
-      optional: true,
-      isMobilePhone: {
-        options: "any",
-        errorMessage: "invalid phone number",
-      },
-    },
-    email: {
-      in: ["body"],
-      optional: true,
-      isEmail: {
-        errorMessage: "invalid email address",
-      },
-      normalizeEmail: true,
+  },
+  street: {
+    in: ["body"],
+    trim: true,
+    escape: true,
+    notEmpty: {
+      bail: true,
+      errorMessage: "Address is required",
     },
   },
+  city: {
+    in: ["body"],
+    trim: true,
+    escape: true,
+    notEmpty: {
+      bail: true,
+      errorMessage: "Address is required",
+    },
+  },
+  postalCode: {
+    in: ["body"],
+    optional: true,
+    isPostalCode: {
+      options: "any",
+      errorMessage: "invalid postal code",
+    },
+  },
+  countryCode: {
+    in: ["body"],
+    isISO31661Alpha2: {
+      errorMessage: "Invalid country code",
+    },
+    notEmpty: {
+      bail: true,
+      errorMessage: "Country code is required",
+    },
+  },
+  phoneNumber: {
+    in: ["body"],
+    optional: true,
+    isMobilePhone: {
+      options: "any",
+      errorMessage: "invalid phone number",
+    },
+  },
+  email: {
+    in: ["body"],
+    optional: true,
+    isEmail: {
+      errorMessage: "invalid email address",
+    },
+    normalizeEmail: true,
+  },
 };
+
+module.exports = propertySchema;
