f691466f8ee65841d1c454d476aa33c61147f2a3
tomason182 <elotro182@hotmail.com>
2024-08-02 15:16:13 -0300

    add: controlador para crear propiedad

--- src/controllers/propertyController.js
+++ src/controllers/propertyController.js
@@ -1,13 +1,73 @@
+const propertySchema = require("../schemas/propertySchema");
+const {
+  checkSchema,
+  validationResult,
+  matchedData,
+} = require("express-validator");
+const { getDd } = require("../config/db_config");
+
 // @desc    Create a property
 // @route   POST /api/v1/property
 // @access  Private
-exports.property_create = async (req, res, next) => {
-  try {
-    res.status(200).json({ msg: "Create a property" });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.property_create = [
+  checkSchema(propertySchema),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+      }
+
+      const {
+        propertyName,
+        street,
+        city,
+        postalCode,
+        countryCode,
+        phoneNumber,
+        email,
+      } = matchedData(req);
+      const userId = req.user._id;
+
+      const Property = {
+        property_name: propertyName,
+        address: [
+          {
+            street: street,
+            city: city,
+            postal_code: postalCode,
+            country_code: countryCode,
+          },
+        ],
+        contact_info: {
+          phone_number: phoneNumber,
+          email: email,
+        },
+        createdBy: userId,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      const db = getDd();
+      const propertyCollection = db.collection("properties");
+
+      const result = await propertyCollection.insertOne(Property);
+
+      if (result) {
+        res
+          .status(200)
+          .json({ msg: "Property created successfully", value: result });
+      } else {
+        res.status(400);
+        throw new Error(
+          "An error occurred when trying to insert a new property"
+        );
+      }
+    } catch (err) {
+      next(err);
+    }
+  },
+];
 
 // @desc    get a property details
 // @route   GET /api/v1/property/:id_property
