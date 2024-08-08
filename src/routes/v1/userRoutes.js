831a6e74227ea07adc9f55daf7f796b76ea48c07
tomason182 <elotro182@hotmail.com>
2024-08-08 09:57:02 -0300

    fix: route for register a new User

--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -8,8 +8,8 @@ const user_controller = require("../../controllers/userController");
 
 /// USER ROUTES ///
 
-// Create a user
-router.post("/", user_controller.user_create);
+// Register a new user
+router.post("/register", user_controller.user_create);
 
 // Authenticate a user
 router.post("/auth", user_controller.user_auth);
