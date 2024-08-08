7f6c5674b1e213d50ceecab606aa678b900bf37a
tomason182 <elotro182@hotmail.com>
2024-08-08 18:44:23 -0300

    fix: remove create Property controller
    As the property is created by default when a user creates an account, and it is set up with null values, is not needed a controller to create a property. We can handle this with update property controller

--- src/controllers/propertyController.js
+++ src/controllers/propertyController.js
@@ -7,69 +7,8 @@ const {
 const { getDb } = require("../config/db_config");
 const { ObjectId } = require("mongodb");
 
-// @desc    Create a property
-// @route   POST /api/v1/property
-// @access  Private
-exports.property_create = [
-  checkSchema(propertySchema),
-  async (req, res, next) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
-      }
-
-      const {
-        propertyName,
-        street,
-        city,
-        postalCode,
-        countryCode,
-        phoneNumber,
-        email,
-      } = matchedData(req);
-      const userId = req.user._id;
-
-      const Property = {
-        property_name: propertyName,
-        address: {
-          street: street,
-          city: city,
-          postal_code: postalCode,
-          country_code: countryCode,
-        },
-        contact_info: {
-          phone_number: phoneNumber,
-          email: email,
-        },
-        createdBy: userId,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-
-      const db = getDb();
-      const propertyCollection = db.collection("properties");
-
-      const result = await propertyCollection.insertOne(Property);
-
-      if (result) {
-        res
-          .status(200)
-          .json({ msg: "Property created successfully", value: result });
-      } else {
-        res.status(400);
-        throw new Error(
-          "An error occurred when trying to insert a new property"
-        );
-      }
-    } catch (err) {
-      next(err);
-    }
-  },
-];
-
 // @desc    get a property details
-// @route   GET /api/v1/property/:id_property
+// @route   GET /api/v1/property/
 // @access  Private
 exports.property_details_get = async (req, res, next) => {
   try {
@@ -108,26 +47,11 @@ exports.property_details_update = [
         return res.status(400).json(errors.array());
       }
 
-      const db = getDb();
-      const propId = ObjectId.createFromHexString(req.params.id); // Tal vez hay que validar el parametro pasado por el cliente (en todos los casos)
-      const propertyCollection = db.collection("properties");
-      const result = await propertyCollection.findOne({ _id: propId });
-
-      if (result === null) {
-        res.status(400);
-        throw new Error("Property not found");
-      }
-
-      console.log(result.createdBy);
-      console.log(req.user._id);
-
-      if (result.createdBy.toString() !== req.user._id.toString()) {
-        // En realidad esta comprobación esta mal. Solo permite actualizar la propiedad al usuario que la creo. Habria que manejar roles.
-        res.status(401);
-        throw new Error("Invalid credentials");
-      }
-
       const data = matchedData(req);
+      const propId = req.user.property_id;
+
+      const db = getDb();
+      const propertyCollection = db.collection("properties");
 
       const filter = { _id: propId };
 
