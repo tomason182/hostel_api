0d413cd41ac85c29a6707e062ac52cef3f1f8ae5
tomason182 <elotro182@hotmail.com>
2024-08-09 17:26:18 -0300

    add: helper to insert user to property

--- src/utils/transactions_operations.js
+++ src/utils/transactions_operations.js
@@ -39,3 +39,51 @@ exports.insertUserPropertyAndAccessControlOnRegister = async (
     await session.endSession();
   }
 };
+
+exports.insertUserToProperty = async (
+  client,
+  dbname,
+  user,
+  role,
+  propertyId
+) => {
+  const session = client.startSession();
+  try {
+    session.startTransaction();
+
+    // Insert new User in users collection
+    const userColl = client.db(dbname).collection("users");
+    const userResult = await userColl.insertOne(user, { session });
+    const userId = userResult.insertedId;
+
+    // Find the property in the access_control collection and append the userId
+    const filter = { property_id: propertyId };
+    const updateDoc = {
+      $push: { access: { user_id: userId, role: role } },
+    };
+    const options = {
+      upsert: false,
+    };
+    const accessControlColl = client.db(dbname).collection("access_control");
+    const accessControlResult = accessControlColl.updateOne(
+      filter,
+      updateDoc,
+      options,
+      { session }
+    );
+
+    if (accessControlResult.matchedCount === 0) {
+      throw new Error("Require to create a property first");
+    }
+
+    await session.commitTransaction();
+    return {
+      msg: `User added successfully. ${accessControlResult.matchedCount} updated documents`,
+    };
+  } catch (err) {
+    await session.abortTransaction();
+    throw new Error("An Error occurred during the transaction", err);
+  } finally {
+    await session.endSession();
+  }
+};
