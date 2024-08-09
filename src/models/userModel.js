38bba22838d44e52f1eab4468cab37233950496f
tomason182 <elotro182@hotmail.com>
2024-08-09 17:57:01 -0300

    fix: Modify constructor name to match db

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,15 +1,15 @@
 const { saltGenerator, hashGenerator } = require("../utils/hash");
 
 class User {
-  constructor(username, password, firstName, lastName, phoneNumber) {
+  constructor(username, password, first_name, last_name, phone_number) {
     (this.username = username),
       (this.salt = saltGenerator(32)),
-      (this.hashedPassword = hashGenerator(password, this.salt)),
-      (this.firstName = firstName),
-      (this.lastName = lastName),
-      (this.contactDetails = {
+      (this.hashed_password = hashGenerator(password, this.salt)),
+      (this.first_name = first_name),
+      (this.last_name = last_name),
+      (this.contact_details = {
         email: username,
-        phoneNumber: phoneNumber || null,
+        phone_number: phone_number || null,
       }),
       (this.createdAt = new Date()),
       (this.updatedAt = new Date());
