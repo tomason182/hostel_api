c3c45fc53cdb83f2c7d715ce1b54b5f0ec7e61cb
tomason182 <elotro182@hotmail.com>
2024-08-09 17:57:43 -0300

    add: Helper function to update a user

--- src/utils/crud_operations.js
+++ src/utils/crud_operations.js
@@ -11,3 +11,31 @@ exports.findOneUser = async (client, dbname, username) => {
     throw new Error(err);
   }
 };
+
+exports.updateOneUser = async (client, dbname, userId, data) => {
+  try {
+    const db = client.db(dbname);
+    const userColl = db.collection("users");
+
+    const filter = { _id: userId };
+    const updateUser = {
+      $set: {
+        first_name: data.first_name,
+        last_name: data.lastName,
+        contact_details: {
+          phone_number: data.phoneNumber,
+          email: data.email,
+        },
+        updatedAt: new Date(),
+      },
+    };
+
+    const result = await userColl.updateOne(filter, updateUser);
+    return result;
+  } catch (err) {
+    throw new Error(
+      "An error ocurred while updating the user information",
+      err
+    );
+  }
+};
