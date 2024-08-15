c5c7f930d1de82fdfab31f235eb36b06ca765e66
tomason182 <elotro182@hotmail.com>
2024-08-15 08:51:16 -0300

    Add: guest model

---
+++ src/models/guestModel.js
@@ -0,0 +1,36 @@
+class Guest {
+  constructor(property_id, first_name, last_name, genre = "undefined") {
+    (this.property_id = property_id),
+      (this.first_name = first_name),
+      (this.last_name = last_name),
+      (this.contact_info = {}),
+      (this.address = {}),
+      (this.identification = {}),
+      (this.genre = genre),
+      (this.created_At = new Date()),
+      (this.updated_At = new Date());
+  }
+
+  setContactInfo(email, phoneNumber) {
+    this.contact_info = {
+      email,
+      phoneNumber,
+    };
+  }
+
+  setAddress(street, city, countryCode, postalCode) {
+    this.address = {
+      street,
+      city,
+      countryCode,
+      postalCode,
+    };
+  }
+
+  setIdentification(idType, idNumber) {
+    this.identification = {
+      idType,
+      idNumber,
+    };
+  }
+}
