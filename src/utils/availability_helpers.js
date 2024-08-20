e328de3b10b8982a1ada6db8c2bd23461044a746
tomason182 <elotro182@hotmail.com>
2024-08-20 18:08:04 -0300

    Fix: Handle error in async function

--- src/utils/availability_helpers.js
+++ src/utils/availability_helpers.js
@@ -5,29 +5,37 @@ exports.checkAvailability = async (
   checkIn,
   checkOut
 ) => {
-  const db = client.db(dbname);
-  const roomTypeColl = db.collections("room_types");
-  const availabilityColl = db.collections("rate_and_availability");
+  try {
+    const db = client.db(dbname);
+    const roomTypeColl = db.collections("room_types");
+    const availabilityColl = db.collections("rate_and_availability");
 
-  const roomType = await roomTypeColl.findOne({ _id: typeRoomId });
-  // Usamos flatMap para obtener todas las camas pertenecientes al tipo de cuarto.
-  const bedsId = roomType.products.flatMap(product => product.beds);
+    const roomType = await roomTypeColl.findOne({ _id: typeRoomId });
 
-  const filter = {
-    _id: { $in: bedsId },
-    availability: {
-      $not: {
-        $elemMatch: {
-          start_date: { $lte: checkOut },
-          end_date: { $gte: checkIn },
+    if (!roomType) {
+      throw new Error("Room type not found");
+    }
+    // Usamos flatMap para obtener todas las camas pertenecientes al tipo de cuarto.
+    const bedsId = roomType.products.flatMap(product => product.beds);
+
+    const filter = {
+      _id: { $in: bedsId },
+      availability: {
+        $not: {
+          $elemMatch: {
+            start_date: { $lte: checkOut },
+            end_date: { $gte: checkIn },
+          },
         },
       },
-    },
-  };
+    };
 
-  // Deberia devolver el listado de camas que estan disponibles para la fecha solicitada.
-  const cursor = await availabilityColl.find(filter);
-  const availability = cursor.toArray();
+    // Deberia devolver el listado de camas que estan disponibles para la fecha solicitada.
+    const cursor = await availabilityColl.find(filter);
+    const availability = cursor.toArray();
 
-  return availability;
+    return availability;
+  } catch (err) {
+    throw err;
+  }
 };
