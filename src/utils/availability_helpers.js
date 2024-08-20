0202f89ff3b87b7aae627fd3df455c2befa2cc70
tomason182 <elotro182@hotmail.com>
2024-08-20 17:40:02 -0300

    add: checkAvailability function
    Funcion que nos permite realizar busquedas en la base de datos en base a la disponibilidad de camas para una fecha (ingreso - egreso) dado

---
+++ src/utils/availability_helpers.js
@@ -0,0 +1,33 @@
+exports.checkAvailability = async (
+  client,
+  dbname,
+  typeRoomId,
+  checkIn,
+  checkOut
+) => {
+  const db = client.db(dbname);
+  const roomTypeColl = db.collections("room_types");
+  const availabilityColl = db.collections("rate_and_availability");
+
+  const roomType = await roomTypeColl.findOne({ _id: typeRoomId });
+  // Usamos flatMap para obtener todas las camas pertenecientes al tipo de cuarto.
+  const bedsId = roomType.products.flatMap(product => product.beds);
+
+  const filter = {
+    _id: { $in: bedsId },
+    availability: {
+      $not: {
+        $elemMatch: {
+          start_date: { $lte: checkOut },
+          end_date: { $gte: checkIn },
+        },
+      },
+    },
+  };
+
+  // Deberia devolver el listado de camas que estan disponibles para la fecha solicitada.
+  const cursor = await availabilityColl.find(filter);
+  const availability = cursor.toArray();
+
+  return availability;
+};
