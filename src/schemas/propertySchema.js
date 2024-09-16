7cbb3b0a5bfc6a410f9737241aba127f864d7910
tomason182 <elotro182@hotmail.com>
2024-09-16 11:34:53 -0300

    fix: set trim and scape at the bottom

--- src/schemas/propertySchema.js
+++ src/schemas/propertySchema.js
@@ -1,32 +1,32 @@
 const propertySchema = {
   propertyName: {
     in: ["body"],
-    trim: true,
-    escape: true,
     notEmpty: {
       bail: true,
       errorMessage: "Property name must not be empty",
     },
+    trim: true,
+    escape: true,
   },
   street: {
     in: ["body"],
     optional: true,
-    trim: true,
-    escape: true,
     isLength: {
       max: 100,
       errorMessage: "Street max length is 100 characters",
     },
+    trim: true,
+    escape: true,
   },
   city: {
     in: ["body"],
     optional: true,
-    trim: true,
-    escape: true,
     isLength: {
       max: 100,
       errorMessage: "City max length is 100 characters",
     },
+    trim: true,
+    escape: true,
   },
   postalCode: {
     in: ["body"],
