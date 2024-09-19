a8da1d2fd6ee64aa081222e39084f09eeae422c0
tomason182 <elotro182@hotmail.com>
2024-09-19 10:30:45 -0300

    add: Set products to room types

--- src/models/roomTypeModel.js
+++ src/models/roomTypeModel.js
@@ -1,17 +1,48 @@
+import { ObjectId } from "mongodb";
 
 class RoomType {
   _id = null;
   property_id = null;
-  constructor(description, type, bathroom = null, max_occupancy, inventory, base_rate, currency) {
-      (this.description = description),
+  constructor(
+    description,
+    type,
+    max_occupancy,
+    inventory,
+    base_rate,
+    currency
+  ) {
+    (this.description = description),
       (this.type = type),
-      (this.bathroom = bathroom),
       (this.max_occupancy = max_occupancy),
       (this.inventory = inventory),
       (this.base_rate = base_rate),
       (this.currency = currency),
-      (this.createdAt = new Date()),
+      (this.products = [])((this.createdAt = new Date())),
       (this.updatedAt = new Date());
+  }
+
+  setProducts() {
+    // Tal vez tengamos que limitar el numero maximo de inventory y max_occupancy en schema.
+    for (let i = 0; i < this.inventory; i++) {
+      let bedsArray = [];
+
+      // Si room type es "privete" le asignamos una sola cama
+      // Si room type es "dorm" la cantidad de camas es igual a max_occupancy
+      if (this.type === "private") {
+        bedsArray = new Array(1);
+      } else {
+        bedsArray = new Array(parseInt(this.max_occupancy));
+      }
+
+      // Agregamos un ID a cada cama
+      const bedsList = bedsArray.map(bed => new ObjectId());
+
+      const roomNum = i.toString();
+      this.products.push({
+        room_name: "Room" + roomNum.padStart(2, "0"),
+        beds: bedsList,
+      });
+    }
   }
 
   get_ID() {
