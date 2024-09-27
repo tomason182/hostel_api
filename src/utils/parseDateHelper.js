fc55599545caacaaac425d4d9d9cdf55b40ec924
tomason182 <elotro182@hotmail.com>
2024-09-27 16:22:06 -0300

    add: parse date with or without hyphen

--- src/utils/parseDateHelper.js
+++ src/utils/parseDateHelper.js
@@ -1,4 +1,12 @@
-export default function parseDateHelper(dateString) {
+export function parseDateWithHyphen(dateString) {
   const [year, month, day] = dateString.split("-").map(Number);
+  return new Date(year, month - 1, day);
+}
+
+export function parseDateOnlyNumbers(dateString) {
+  const year = Number(dateString.substring(0, 4));
+  const month = Number(dateString.substring(4, 6));
+  const day = Number(dateString.substring(6, 8));
+
   return new Date(year, month - 1, day);
 }
