ce3e650e729e0fc34cbddab5a2d5ebca349b210a
tomason182 <elotro182@hotmail.com>
2024-09-30 14:27:07 -0300

    add: handleCalendarReservations

--- src/utils/reservationHelpers.js
+++ src/utils/reservationHelpers.js
@@ -104,3 +104,60 @@ exports.findReservationsByDateRange = async (
     throw new Error(err);
   }
 };
+
+exports.handleCalendarReservations = async (
+  client,
+  dbname,
+  propertyId,
+  fromDate,
+  toDate
+) => {
+  try {
+    const db = client.db(dbname);
+    const reservationColl = db.collection("reservations");
+
+    const query = {
+      property_id: propertyId,
+      check_in: { $lte: toDate },
+      check_out: { $gt: fromDate },
+    };
+
+    const aggregation = [
+      { $match: query },
+      {
+        $lookup: {
+          from: "guests",
+          localField: "guest_id",
+          foreignField: "_id",
+          as: "guest_info",
+        },
+      },
+      {
+        $unwind: "$guest_info",
+      },
+      {
+        $project: {
+          _id: 1,
+          room_type_id: 1,
+          number_of_guest: 1,
+          total_price: 1,
+          reservation_status: 1,
+          assigned_beds: 1,
+          check_in: 1,
+          check_out: 1,
+          "guest_info.full_name": {
+            $concat: ["$guest_info.first_name", " ", "$guest_info.last_name"],
+          },
+        },
+      },
+    ];
+
+    const reservationsList = await reservationColl
+      .aggregate(aggregation)
+      .toArray();
+
+    return reservationsList;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
