d3d0f587f384d36f1673ce658fd4c8ece39ef58e
tomason182 <elotro182@hotmail.com>
2024-10-29 19:39:22 -0300

    fix: remove double check strong password

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -248,7 +248,6 @@ const userChangePassSchema = {
   },
 };
 
-
 const usernameSchema = {
   username: {
     in: ["body"],
@@ -258,7 +257,6 @@ const usernameSchema = {
     },
   },
 };
-
 
 const userChangePassSchema2 = {
   newPassword: {
@@ -285,25 +283,8 @@ const userChangePassSchema2 = {
   },
   repeatNewPassword: {
     in: ["body"],
-    isStrongPassword: {
-      options: {
-        minLength: 14,
-        minLowerCase: 4,
-        minUppercase: 2,
-        minNumbers: 2,
-        minSymbols: 2,
-      },
-    },
-    custom: {
-      options: value => {
-        if (/\s/.test(value)) {
-          throw new Error("Password should not contain white spaces");
-        }
-        return true;
-      },
-    },
-    errorMessage:
-      "Password should contain at least 14 characters, 4 lowercase, 2 uppercase, 2 numbers and 2 symbols ",
+    trim: true,
+    escape: true,
   },
 };
 
