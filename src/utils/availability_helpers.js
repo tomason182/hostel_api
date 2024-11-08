618798b70f7f61e25b4c223012be39631a27ee4a
tomason182 <elotro182@hotmail.com>
2024-11-08 18:53:41 -0300

    add: bedassigment helper function

--- src/utils/availability_helpers.js
+++ src/utils/availability_helpers.js
@@ -175,91 +175,125 @@ exports.pullOverlappingElementsFromArray = async (
   }
 };
 
-exports.bedsAssignment = async (
-  client,
-  dbname,
-  roomTypes,
-  reservationsList
-) => {
+exports.bedsAssignment = async (client, dbname, typeRoomId, reservation) => {
   try {
-    let reservationsToUpdate = [];
+    const db = client.db(dbname);
+    const roomTypeColl = db.collection("room_types");
+    const reservationsColl = db.collection("reservations");
 
-    for (let roomType of roomTypes) {
-      const totalBeds = roomType.products.flatMap(product => product.beds);
+    const roomType = await roomTypeColl.findOne({ _id: typeRoomId });
 
-      // Obtener las reservas para este tipo de cuarto
-      const reservationsByRoomType = reservationsList.filter(r =>
-        r.room_type_id.equals(roomType._id)
-      );
-
-      // Obtener las reservas que NO tienen cama asignada
-      const reservationsWithNoBedsAssigned = reservationsByRoomType.filter(
-        r => r.assigned_beds.length === 0
-      );
-
-      // Obtener las camas ocupadas
-      const occupiedBeds = reservationsByRoomType.flatMap(r => r.assigned_beds);
-
-      // Camas disponibles
-      let availableBeds = totalBeds.filter(
-        bed => !occupiedBeds.some(occupied => occupied.equals(bed))
-      );
-
-      if (roomType.type === "dorm") {
-        // Asignar camas a reservas de dormitorios
-        const updateDormReservations = reservationsWithNoBedsAssigned.map(r => {
-          const numberOfGuest = r.number_of_guest;
-          if (availableBeds.length >= numberOfGuest) {
-            r.assigned_beds = availableBeds.slice(0, numberOfGuest);
-            availableBeds = availableBeds.slice(numberOfGuest); // Remover las camas asignadas
-          }
-
-          return r;
-        });
-        reservationsToUpdate = [
-          ...reservationsToUpdate,
-          ...updateDormReservations,
-        ];
-      } else {
-        // Asignar camas a habitaciones privadas
-        const updatePrivateReservations = reservationsWithNoBedsAssigned.map(
-          r => {
-            if (availableBeds.length > 0) {
-              r.assigned_beds.push(availableBeds[0]);
-              availableBeds = availableBeds.slice(1);
-            }
-
-            return r;
-          }
-        );
-        reservationsToUpdate = [
-          ...reservationsToUpdate,
-          ...updatePrivateReservations,
-        ];
-      }
+    if (!roomType) {
+      throw new Error("Room type not found");
     }
 
-    // actualizar las reservas.
-    const db = client.db(dbname);
-    const reservationColl = db.collection("reservations");
+    // Usamos flatMap para obtener todas las camas pertenecientes al tipo de cuarto.
+    const totalBeds = roomType.products.flatMap(product => product.beds);
 
-    const updatePromises = reservationsToUpdate.map(reservation => {
-      const filter = {
-        _id: reservation._id,
-      };
+    const checkIn = reservation.check_in;
+    const checkOut = reservation.check_out;
 
-      const updateDoc = {
-        $set: {
-          assigned_beds: reservation.assigned_beds,
-        },
-      };
+    // Filtramos las reservas para obtener solo las que caen dentro del rango check in - check out
 
-      return reservationColl.updateOne(filter, updateDoc);
-    });
+    const filter = {
+      room_type_id: { $eq: typeRoomId },
+      reservation_status: { $nin: ["canceled", "no_show"] },
+      check_in: { $lt: checkOut },
+      check_out: { $gt: checkIn },
+    };
 
-    await Promise.all(updatePromises);
+    const options = {
+      projection: {
+        check_in: 1,
+        check_out: 1,
+        number_of_guest: 1,
+        assigned_beds: 1,
+      },
+    };
 
-    return { msg: "Beds added successfully" };
+    const reservationsList = await reservationsColl
+      .find(filter, options)
+      .toArray();
+
+    // Segundo paso: Obtener las reservas que se solapan, pero del lado izquierdo de rango.
+    const overlappingReservationsBeforeCurrent = reservationsList.filter(
+      r =>
+        new Date(r.check_out) > new Date(checkIn) &&
+        new Date(r.check_in) < new Date(checkOut) &&
+        new Date(r.check_in) <= new Date(checkIn)
+    );
+
+    // Tercer paso: Obtener las camas ocupadas del lado izquierdo.
+    const occupiedBedsBefore = overlappingReservationsBeforeCurrent.flatMap(
+      r => r.assigned_beds
+    );
+
+    // Cuarto paso: Obtener las camas disponibles antes
+    const availableBedsBefore = totalBeds.filter(
+      bed => !occupiedBedsBefore.some(occupied => occupied.equals(bed))
+    );
+
+    // Quinto paso: Obtener las camas disponibles despues
+    const overlappingReservationsAfterCurrent = reservationsList.filter(
+      r =>
+        new Date(r.check_out) > new Date(checkIn) &&
+        new Date(r.check_in) < new Date(checkOut) &&
+        new Date(r.check_in) > new Date(checkIn)
+    );
+
+    const occupiedBedsAfter = overlappingReservationsAfterCurrent.flatMap(
+      r => r.assigned_beds
+    );
+
+    const availableBedsAfter = totalBeds.filter(
+      bed => !occupiedBedsAfter.some(occupied => occupied.equals(bed))
+    );
+
+    //Sexto paso: Obtener las camas disponibles en ambos lados
+    const availableBeds = availableBedsBefore.filter(bed =>
+      availableBedsAfter.some(available => available.equals(bed))
+    );
+
+    // Septimo paso: Si hay camas disponibles en ambos lados se asignan. Sino hay que reacomodar todas las reservas de adelante.
+    const numberOfGuest = reservation.number_of_guest;
+
+    let assignedBeds = [];
+    if (roomType.type === "dorm" && availableBeds.length >= numberOfGuest) {
+      for (let i = 0; i < numberOfGuest; i++) {
+        assignedBeds.push(availableBeds[i]);
+      }
+    } else if (roomType.type === "private" && availableBeds.length >= 1) {
+      assignedBeds.push(availableBeds[0]);
+    }
+
+    if (assignedBeds.length > 0) {
+      reservation.setAssignedBeds(assignedBeds);
+      return { reservationToAdd: reservation, reservationsToUpdate: [] };
+    }
+
+    // Si ninguna cama fue agregada seleccionamos las camas libres del primer tramo y las asignamos
+    if (roomType.type === "dorm") {
+      for (let i = 0; i < numberOfGuest; i++) {
+        assignedBeds.push(availableBedsBefore[i]);
+      }
+    } else {
+      assignedBeds.push(availableBedsBefore[0]);
+    }
+
+    reservation.setAssignedBeds(assignedBeds);
+
+    // Incorporamos la nueva reserva a la lista de reservas traidas de la base de datos
+
+    let reservationsToUpdate = [];
+
+    for (let bed in assignedBeds) {
+      const duplicateReservation = reservationsList.filter(
+        r => r.assigned_beds === bed
+      );
+      reservationsToUpdate.push(...duplicateReservation);
+    }
+
+    return { reservationToAdd: reservation, reservationsToUpdate };
   } catch (err) {
     throw new Error(err);
   }
